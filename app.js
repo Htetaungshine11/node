86a2d751371d072e0b29c623da70bc3dc4e1f591
Your Name <you@example.com>
2021-05-20 01:27:12 +0630

    fda

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const path = require("path");
+const http = require("https");
 const jsondata = require("./package.json");
+const fs = require("fs");
+var privateKey = fs.readFileSync("./sslcert/selfsigned.key", "utf8");
+var certificate = fs.readFileSync("./sslcert/selfsigned.crt", "utf8");
+
+var credentials = { key: privateKey, cert: certificate };
 app = express();
 
 app.use(express.static(path.join(__dirname, "templates")));
 
 app.get("/home", function (req, res) {
-  res.send(toString(req));
+  res.send(jsondata);
 });
-app.listen(5000, "0.0.0.0");
+
+http.createServer(credentials, app).listen(8433, "0.0.0.0");
